ccb316521be22e7018655d3e5d9f71a403fa96a1
morris <morrisndeto161@gmail.com>
2023-11-29 21:54:37 +0300

    finished project

---
+++ app/src/main/java/com/firstapp/myquizapplication/Questionanswer.java
@@ -0,0 +1,22 @@
+package com.firstapp.myquizapplication;
+
+public class Questionanswer {
+
+    public static  String question[]={
+            "Which company owns the android?",
+            "Which one is not a programming language?",
+            "Where you are watching this video?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"}
+    };
+
+    public static String correctchoices[]={
+       "Google",
+       "Notepad",
+       "Youtube"
+    };
+}
